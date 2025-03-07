a6086797f1d64ea046ff2a4ccc7d622c55296860
giggi27 <giggi.schiavo@gmail.com>
2025-03-07 17:33:45 +0100

    second

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,33 @@ import { IoIosArrowForward } from "react-icons/io";
 import { LuSettings } from "react-icons/lu";
 import { IoNotifications } from "react-icons/io5";
 import { FaRegCircle } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
+
 
 function App() {
   return (
-    <>
+    <><div className="bar-wrapper">
+      <div className="sidebar-container">
+
+
+      </div>
       <div className="navbar-container">
         <div className="navigation-container">
           <GrHomeRounded size={20} />
           <IoIosArrowForward size={20} />
           <p className="prova">Dashboard</p>
         </div>
-        <div className="search-bar-container"></div>
+         <input type="search" className="search-bar-container" placeholder="Cerca"/>
+          {/* <BiSearchAlt size={20} />
+          <p>Search</p> */}
+        
         <div className="right-content-container">
           {" "}
           <LuSettings size={20} />
           <IoNotifications size={20} />
           <div className="profile-img-container"></div>
         </div>
+      </div>
       </div>
     </>
   );
